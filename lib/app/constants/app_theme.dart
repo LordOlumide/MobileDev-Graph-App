d09c4ea9a0bdbda92ab70dd3758daaf07d9fc2b6
LordOlumide <olumideolubosede@yahoo.com>
2023-01-11 22:13:54 +0100

    setting up

---
+++ lib/app/constants/app_theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+    const AppTheme._();
+}
--- lib/graph_app.dart
+++ lib/graph_app.dart
@@ -1,2 +1,17 @@
 import 'package:flutter/material.dart';
+import '../UI/screens/home_screen.dart';
 
+class GraphApp extends StatelessWidget {
+  const GraphApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Graph App',
+        initialRoute: 'Home screen',
+        routes: {
+            'Home screen': 
+        }
+    );
+  }
+}
